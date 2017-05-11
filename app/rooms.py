6774cd98047011b96afcf5394ea26a298322d7a5
Jeffkungu <Jeffkungu@gmail.com>
2017-05-11 14:03:44 +0300

    Adds gitignore files

--- app/rooms.py
+++ app/rooms.py
@@ -10,7 +10,13 @@ class Office(Room):
         super(Office, self).__init__(room_name=room_name, room_type = "OFFICE", max_capacity = 6, )
         self.occupants = []
 
+    def is_vacant(self):
+        return len(self.occupants) !=  self.max_capacity    
+
 class LivingSpace(Room):
     def __init__(self, room_name):
-        super(LivingSpace, self).__init__(room_name=room_name, room_type = "LIVING SPACE", max_capacity = 4)
+        super(LivingSpace, self).__init__(room_name=room_name, room_type = "LIVINGSPACE", max_capacity = 4)
         self.occupants = []
+
+    def is_vacant(self):
+        return len(self.occupants) !=  self.max_capacity  
